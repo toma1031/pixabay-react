756a2579440fd6722ca14f5f65abbcb618855c37
Akinori Toma <akinoritoma@appletoma.home>
2020-09-24 03:22:20 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios'
+import SearchBar from './components/SearchBar'
+import ImageList from './components/ImageList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import logo from './logo.png';
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const ApiKey = process.env.REACT_APP_PIXABAY_APIKEY;
+  const onSerchSubmit = async (term) => {
+    try {
+      const params={
+        key: ApiKey,
+        q: term,
+      };
+      const response = await axios.get('https://pixabay.com/api/', {params});
+      setImages(response.data.hits);
+      if (response.data.total === 0) {
+        window.alert('Can not find it');
+      }
+    }catch{
+      window.alert('Faild to get it');
+    }
+  };
+  return(
+    <div className='ui container' style={{marginTop: '20px' }}>
+      <img src={logo} alt='pixabay-logo' className='pixabay-logo' />
+      <SearchBar onSubmit={onSerchSubmit} />
+      <ImageList images={images} />
     </div>
   );
-}
+};
 
 export default App;
